4b777dcae13f8602a1252d8511ad130c823e506d
Brian Chang <bchanged@gmail.com>
2011-07-08 19:00:03 -0700

    load antlibs based on .antlib files located under gw/vark/antlibs/ of modules, rather than from antlibs.properties

--- aardvark/src/gw/vark/typeloader/AntlibTypeLoader.java
+++ aardvark/src/gw/vark/typeloader/AntlibTypeLoader.java
@@ -1,5 +1,7 @@
 package gw.vark.typeloader;
 
+import gw.fs.IDirectory;
+import gw.fs.IFile;
 import gw.lang.reflect.IType;
 import gw.lang.reflect.ITypeLoader;
 import gw.lang.reflect.TypeLoaderBase;
@@ -10,43 +12,43 @@ import gw.util.StreamUtil;
 import gw.util.concurrent.LazyVar;
 import gw.vark.Aardvark;
 import org.apache.tools.ant.Project;
-import org.apache.tools.ant.launch.Locator;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.io.Reader;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Set;
 
 public class AntlibTypeLoader extends TypeLoaderBase implements ITypeLoader{
   public static final String GW_VARK_TASKS_PACKAGE = "gw.vark.antlibs.";
+  private static final String GW_VARK_TASKS_PATH = "gw/vark/antlibs";
   private static final String ANT_ANTLIB_SYMBOL = "Ant";
   private static final String ANT_ANTLIB_RESOURCE = "org/apache/tools/ant/taskdefs/defaults.properties";
-  private static final String ANTLIBS_PROPERTIES = "antlibs.properties";
 
   private LazyVar<HashMap<String, IType>> _types = new LazyVar<HashMap<String, IType>>(){
     @Override
     protected HashMap<String, IType> init()
     {
-      Properties antlibs = new Properties();
-      File classSource = Locator.getClassSource(AntlibTypeLoader.class);
-      File antlibsFile = findAntlibsProperties(classSource.isDirectory() ? classSource : classSource.getParentFile());
-      if (antlibsFile != null && antlibsFile.exists()) {
-        Aardvark.getProject().log("found user-defined antlibs: " + antlibsFile.getPath(), Project.MSG_VERBOSE);
-        readProperties(antlibsFile, antlibs);
+      HashMap<String, String> antlibs = new HashMap<String, String>();
+      for (IDirectory sourceEntry : TypeSystem.getCurrentModule().getResourceAccess().getSourceEntries()) {
+        IDirectory antlibsDir = sourceEntry.dir(GW_VARK_TASKS_PATH);
+        if (antlibsDir.exists()) {
+          for (IFile file : antlibsDir.listFiles()) {
+            if ("antlib".equals(file.getExtension())) {
+              antlibs.put(file.getBaseName(), readFile(file).trim());
+            }
+          }
+        }
       }
       antlibs.put(ANT_ANTLIB_SYMBOL, ANT_ANTLIB_RESOURCE);
 
       HashMap<String, IType> types = new HashMap<String, IType>();
-      for( Map.Entry entry : antlibs.entrySet() )
+      for( Map.Entry<String, String> entry : antlibs.entrySet() )
       {
-        String antlibName = (String) entry.getKey();
-        String antlibResource = (String) entry.getValue();
+        String antlibName = entry.getKey();
+        String antlibResource = entry.getValue();
         Aardvark.getProject().log("loading antlib " + antlibName + " (" + antlibResource + ")", Project.MSG_VERBOSE);
         String typeName = GW_VARK_TASKS_PACKAGE + antlibName;
         types.put( typeName, TypeSystem.getOrCreateTypeReference( new AntlibType( typeName, antlibResource, AntlibTypeLoader.this ) ) );
@@ -83,26 +85,16 @@ public class AntlibTypeLoader extends TypeLoaderBase implements ITypeLoader{
     return true;
   }
 
-  private static File findAntlibsProperties(File dir) {
-    if (dir == null) {
-      return null;
-    }
-    if (new File(dir, "bin").exists()) {
-      return new File(dir, ANTLIBS_PROPERTIES);
-    }
-    return findAntlibsProperties(dir.getParentFile());
-  }
-
-  private static void readProperties(File antlibsFile, Properties antlibs) {
-    InputStream in = null;
+  private static String readFile(IFile file) {
+    Reader reader = null;
     try {
-      in = new FileInputStream(antlibsFile);
-      antlibs.load(in);
+      reader = StreamUtil.getInputStreamReader(file.openInputStream());
+      return StreamUtil.getContent(reader);
     } catch (IOException e) {
       throw GosuExceptionUtil.forceThrow(e);
     } finally {
       try {
-        StreamUtil.close(in);
+        StreamUtil.close(reader);
       } catch (IOException e) { }
     }
   }
