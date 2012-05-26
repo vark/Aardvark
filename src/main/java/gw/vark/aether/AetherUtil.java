22f95fb4a6128b8a104abb066d78778e9ebf6d46
Brian Chang <bchang@guidewire.com>
2012-05-25 17:17:53 -0700

    add remote repositories as configured in the pom take MavenScopeCategory as params rather than strings

--- src/main/java/gw/vark/aether/AetherUtil.java
+++ src/main/java/gw/vark/aether/AetherUtil.java
@@ -1,16 +1,18 @@
 package gw.vark.aether;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.Repository;
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.Task;
 import org.apache.tools.ant.types.Path;
 import org.apache.tools.ant.types.resources.FileResource;
+import org.codehaus.plexus.interpolation.reflection.ReflectionValueExtractor;
 import org.sonatype.aether.ant.tasks.Install;
 import org.sonatype.aether.ant.tasks.Resolve;
-import org.sonatype.aether.ant.types.Artifact;
-import org.sonatype.aether.ant.types.Dependencies;
-import org.sonatype.aether.ant.types.Pom;
+import org.sonatype.aether.ant.types.*;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 /**
@@ -19,13 +21,19 @@ import java.util.Iterator;
 public class AetherUtil {
 
   private final Project _project;
+  private final Pom _pom;
 
   public AetherUtil(Project project) {
-    _project = project;
+    this(project, null);
   }
 
-  public FileList resolve(Dependencies dependencies, String scopes) {
-    Path path = resolveToPath(dependencies, scopes);
+  public AetherUtil(Project project, Pom pom) {
+    _project = project;
+    _pom = pom;
+  }
+
+  public FileList resolve(Dependencies dependencies, MavenScopeCategory scopeCategory) {
+    Path path = resolveToPath(dependencies, scopeCategory);
     FileList list = new FileList();
     for (Iterator it = path.iterator(); it.hasNext();) {
       FileResource file = (FileResource) it.next();
@@ -34,27 +42,27 @@ public class AetherUtil {
     return list;
   }
 
-  public void resolveToDir(Dependencies dependencies, String scopes, File dir, String layout) {
-    Resolve resolveTask = initTask(new Resolve(), "resolve");
+  public void resolveToDir(Dependencies dependencies, MavenScopeCategory scopeCategory, File dir, String layout) {
+    Resolve resolveTask = newResolve();
     resolveTask.addDependencies(dependencies);
 
     Resolve.Files files = resolveTask.createFiles();
     files.setProject(_project);
-    files.setScopes(scopes);
+    files.setScopes(scopeCategory.getExpanded());
     files.setDir(dir);
     files.setLayout(layout);
 
     resolveTask.execute();
   }
 
-  public Path resolveToPath(Dependencies dependencies, String scopes) {
-    Resolve resolveTask = initTask(new Resolve(), "resolve");
+  public Path resolveToPath(Dependencies dependencies, MavenScopeCategory scopeCategory) {
+    Resolve resolveTask = newResolve();
     resolveTask.addDependencies(dependencies);
 
     Resolve.Path path = resolveTask.createPath();
     path.setProject(_project);
     path.setRefId("tmp.path");
-    path.setScopes(scopes);
+    path.setScopes(scopeCategory.getExpanded());
 
     resolveTask.execute();
     return (Path) _project.getReference("tmp.path");
@@ -71,6 +79,30 @@ public class AetherUtil {
       installTask.addArtifact(artifact);
     }
     installTask.execute();
+  }
+
+  private Resolve newResolve() {
+    Resolve resolveTask = initTask(new Resolve(), "resolve");
+    if (_pom != null) {
+      Model model = _pom.getModel(_pom);
+      try {
+        Object o = ReflectionValueExtractor.evaluate("project.repositories", model);
+        if (o != null) {
+          ArrayList pomRepos = (ArrayList) o;
+          for (Object pomRepoObj : pomRepos) {
+            Repository pomRepo = (Repository) pomRepoObj;
+            RemoteRepository remoteRepo = new RemoteRepository();
+            remoteRepo.setId(pomRepo.getId());
+            remoteRepo.setUrl(pomRepo.getUrl());
+            resolveTask.addRemoteRepo(remoteRepo);
+          }
+        }
+      }
+      catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    }
+    return resolveTask;
   }
 
   private <T extends Task> T initTask(T task, String name) {
