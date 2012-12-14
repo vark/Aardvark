1fbe0ca74e5356dda65122b216c0d01948c79afe
Ivan Dubrov <idubrov@guidewire.com>
2012-12-14 11:57:45 -0800

    Correct line numbers for embedded Gosu programs
    Attempt to provide actual line numbers inside build.xml in Gosu error messages.

--- aardvark-core/src/main/java/gw/vark/task/GosuInvokerTask.java
+++ aardvark-core/src/main/java/gw/vark/task/GosuInvokerTask.java
@@ -16,12 +16,21 @@
 
 package gw.vark.task;
 
+import gw.config.CommonServices;
 import gw.lang.parser.GosuParserFactory;
-import gw.lang.parser.IGosuParser;
+import gw.lang.parser.IFileContext;
+import gw.lang.parser.IGosuProgramParser;
+import gw.lang.parser.IParseResult;
+import gw.lang.parser.ITypeUsesMap;
+import gw.lang.parser.ParserOptions;
 import gw.lang.parser.StandardSymbolTable;
 import gw.lang.parser.exceptions.IEvaluationException;
+import gw.vark.AardvarkProgram;
 import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Location;
 import org.apache.tools.ant.Task;
+
+import java.util.List;
 
 @SuppressWarnings({"UnusedDeclaration"})
 public class GosuInvokerTask extends Task {
@@ -32,24 +41,70 @@ public class GosuInvokerTask extends Task {
     _expression.append(getProject().replaceProperties(text));
   }
 
+  @Override
+  public void init() throws BuildException {
+    // Append N-1 empty lines, to match Gosu line numbers with build.xml.
+    Location loc = getLocation();
+    if (loc != null) {
+      for (int i = 0; i < getLocation().getLineNumber() - 1; i++) {
+        _expression.append('\n');
+      }
+    }
+  }
+
   public void execute() throws BuildException {
     if (_expression.length() == 0) {
       throw new BuildException("no expression to evaluate");
     }
 
     try {
-      IGosuParser parser = GosuParserFactory.createParser( _expression.toString(), new StandardSymbolTable( true ) );
-      parser.parseExpOrProgram( null, false, true ).evaluate();
+      IGosuProgramParser parser = GosuParserFactory.createProgramParser();
+
+      String script = _expression.toString();
+      IParseResult result =
+              parser.parseExpressionOrProgram(script,
+                      new StandardSymbolTable(true), createParserOptions());
+      result.getProgram().evaluate(null);
     } catch (IEvaluationException e) {
       if (e.getCause() != null && e.getCause() instanceof BuildException) {
         // TODO - blc - the Gosu stack trace could be useful here
         throw (BuildException) e.getCause();
-      }
-      else {
+      } else {
         throw e;
       }
     } catch (Exception e) {
       throw new BuildException(e);
     }
+  }
+
+  private ParserOptions createParserOptions() {
+    IFileContext fileContext = new IFileContext() {
+      @Override
+      public String getContextString() {
+        return null;
+      }
+
+      @Override
+      public String getClassName() {
+        return null;
+      }
+
+      @Override
+      public String getFilePath() {
+        return getLocation().getFileName();
+      }
+    };
+
+    List<String> packages = AardvarkProgram.getDefaultTypeUsesPackages();
+    ITypeUsesMap typeUses = CommonServices.getGosuIndustrialPark().createTypeUsesMap(packages);
+    for (String aPackage : packages) {
+      typeUses.addToDefaultTypeUses(aPackage);
+    }
+
+    ParserOptions options =
+            new ParserOptions()
+                    .withFileContext(fileContext)
+                    .withTypeUsesMap(typeUses);
+    return options;
   }
 }
